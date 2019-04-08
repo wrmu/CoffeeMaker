3c01a7978882c469fedb0985c08fe3388c8b69df
wuruimin <wrst@qq.com>
2019-04-08 18:55:16 +0800

    V1

--- src/Entity/CoffeeMachine.java
+++ src/Entity/CoffeeMachine.java
@@ -1,7 +1,6 @@
 package Entity;
 
 import State.MachineState;
-import State.impl.*;
 
 
 public class CoffeeMachine {
@@ -9,11 +8,6 @@ public class CoffeeMachine {
     final static int MEDIUM = 2;
     final static int LIGHT = 3;
 
-    MachineState boilerEmpty;
-    MachineState boilerNotEmpty;
-    MachineState plateEmpty;
-    MachineState potEmpty;
-    MachineState potNotEmpty;
 
     MachineState machineState;
 
@@ -21,35 +15,19 @@ public class CoffeeMachine {
     int level = 0;
 
     public CoffeeMachine(int boiler) {
-        boilerEmpty = new BoilerEmpty(this);
-        boilerNotEmpty = new BoilerNotEmpty(this);
-        plateEmpty = new PlateEmpty(this);
-        potEmpty = new PotEmpty(this);
-        potNotEmpty = new PotNotEmpty(this);
+
         this.count = boiler;
         this.level = MEDIUM;
 
-        if (boiler > 0) {
-            machineState = boilerNotEmpty;
-        } else {
-            machineState = boilerEmpty;
-        }
+
     }
 
     public CoffeeMachine(int boiler, int level) {
-        boilerEmpty = new BoilerEmpty(this);
-        boilerNotEmpty = new BoilerNotEmpty(this);
-        plateEmpty = new PlateEmpty(this);
-        potEmpty = new PotEmpty(this);
-        potNotEmpty = new PotNotEmpty(this);
+
         this.count = boiler;
         this.level = level;
 
-        if (boiler > 0) {
-            machineState = boilerNotEmpty;
-        } else {
-            machineState = boilerEmpty;
-        }
+
     }
 
     public MachineState getMachineState() {
@@ -60,31 +38,4 @@ public class CoffeeMachine {
         this.machineState = machineState;
     }
 
-    public MachineState getBoilerEmpty() {
-        return boilerEmpty;
-    }
-
-    public MachineState getBoilerNotEmpty() {
-        return boilerNotEmpty;
-    }
-
-    public MachineState getPlateEmpty() {
-        return plateEmpty;
-    }
-
-    public MachineState getPotEmpty() {
-        return potEmpty;
-    }
-
-    public MachineState getPotNotEmpty() {
-        return potNotEmpty;
-    }
-
-    public void BeginBrew() {
-
-    }
-
-    public void interruptBrew() {
-        machineState.boilerHeaterOff();
-    }
 }
--- src/test/TestMachine.java
+++ src/test/TestMachine.java
@@ -1,11 +1,16 @@
 package test;
 
+import Entity.CoffeeMachine;
+import State.MachineState;
 import org.junit.Test;
 
 public class TestMachine {
     @Test
     public void testBrewCoffee() {
-
+        CoffeeMachine machine = new CoffeeMachine(12);
+        MachineState machineState = machine.getMachineState();
+        String s = machineState.getClass().toString();
+        System.out.println(s);
     }
 
     @Test
